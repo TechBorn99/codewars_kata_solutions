7a52318ea64d92f7d2fd4f2dfaeff24d895ce95a
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-02 12:06:24 +0200

    Added "Round up to the next multiple of 5" kata solution

---
+++ 7_kyu_katas/round_up_to_the_next_multiple_of_5.py
@@ -0,0 +1,37 @@
+"""
+Kata description:
+Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.
+"""
+
+
+def round_to_next5(n):
+    while n % 5 != 0:
+        n += 1
+    return n
+
+
+if __name__ == "__main__":
+    print(round_to_next5(0))
+    print(round_to_next5(2))
+    print(round_to_next5(3))
+    print(round_to_next5(12))
+    print(round_to_next5(21))
+    print(round_to_next5(30))
+    print(round_to_next5(-2))
+    print(round_to_next5(-5))
