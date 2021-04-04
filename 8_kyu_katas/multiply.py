fa2ce52d0335f7902f57adad2152328973486d69
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-04 15:38:02 +0200

    Added "Multiply" kata solution

---
+++ 8_kyu_katas/multiply.py
@@ -0,0 +1,16 @@
+"""
+Kata description:
+
+This code does not execute properly. Try to figure out why.
+"""
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print(multiply(1, 3))
+    print(multiply(10, 4))
+    print(multiply(4, 5))
+    print(multiply(6, 13))
