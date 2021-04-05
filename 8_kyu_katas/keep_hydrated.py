b7255d81cd2609308c8b6f6084125998324a2952
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-05 11:32:25 +0200

    Added "Keep Hydrated!" kata solution

---
+++ 8_kyu_katas/keep_hydrated.py
@@ -0,0 +1,32 @@
+"""
+Kata description:
+
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest
+value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5
+"""
+
+
+def litres(time):
+    return time // 2
+
+
+if __name__ == '__main__':
+    print(litres(2))
+    print(litres(1.4))
+    print(litres(12.3))
+    print(litres(0.82))
+    print(litres(11.8))
+    print(litres(1787))
+    print(litres(0))
