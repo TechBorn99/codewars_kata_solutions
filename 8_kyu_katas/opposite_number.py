b63e7b1760375de5618f557f011de563c225ae64
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-06 12:53:26 +0200

    Added "Opposite number" kata solution

---
+++ 8_kyu_katas/opposite_number.py
@@ -0,0 +1,21 @@
+"""
+Kata description:
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+"""
+
+
+def opposite(number):
+    return -number
+
+
+if __name__ == "__main__":
+    print(opposite(1))
+    print(opposite(14))
+    print(opposite(-34))
