04e3f1e766e6fce7426d510b3d844e63790632ac
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-13 20:40:38 +0200

    Added "Grasshopper - Function syntax debugging" kata solution

---
+++ 8_kyu_katas/grasshopper-function_syntax_debugging.py
@@ -0,0 +1,16 @@
+"""
+Kata description:
+
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and
+fix them.
+"""
+
+
+def main(verb, noun):
+    return verb + noun
+
+
+if __name__ == "__main__":
+    print(main('take ', 'item'))
+    print(main('use ', 'sword'))
