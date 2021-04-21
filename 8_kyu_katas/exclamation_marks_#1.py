489d96c0f3ef2570760bebf919e4520b42a4a127
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-21 23:53:55 +0200

    Added "Exclamation marks series #1: Remove a exclamation mark from the end of string" kata solution

---
+++ 8_kyu_katas/exclamation_marks_#1.py
@@ -0,0 +1,32 @@
+"""
+Kata description:
+
+Description:
+Remove a exclamation mark from the end of string. For a beginner kata, you can assume that the input data is always a
+string, no need to verify it.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi!!"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+Note
+Please don't post issue about difficulty or duplicate.
+"""
+
+
+def remove(s):
+    if not s:
+        return ''
+    return s[:len(s) - 1] if s[len(s) - 1] == '!' else s[:len(s)]
+
+
+if __name__ == "__main__":
+    print(remove("Hi!"))
+    print(remove("Hi! Hi!"))
+    print(remove("Hi!!!"))
+    print(remove("!Hi!"))
+    print(remove("Hi"))
+    print(remove("!Hi"))
