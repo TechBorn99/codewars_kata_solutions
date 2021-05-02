75eca59ef41c7d1402bda15bd9e4f3fd17eb0c85
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-05-02 18:34:51 +0200

    Added "Two to One" kata solution

---
+++ 7_kyu_katas/two_to_one.py
@@ -0,0 +1,24 @@
+"""
+Kata description:
+
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible,
+containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+"""
+
+
+def longest(s1, s2):
+    return ''.join(sorted(set(s1 + s2)))
+
+
+if __name__ == "__main__":
+    print(longest("xyaabbbccccdefww", "xxxxyyyyabklmopq"))
+    print(longest("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz"))
+
