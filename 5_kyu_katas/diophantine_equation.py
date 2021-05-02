2779d61a7a8ef788194f38973fe454395184afc3
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-05-02 18:38:18 +0200

    Added "Diophantine Equation" kata solution

---
+++ 5_kyu_katas/diophantine_equation.py
@@ -0,0 +1,39 @@
+"""
+Kata description:
+
+In mathematics, a Diophantine equation is a polynomial equation, usually with two or more unknowns, such that only the
+integer solutions are sought or studied.
+
+In this kata we want to find all integers x, y (x >= 0, y >= 0) solutions of a diophantine equation of the form:
+
+x2 - 4 * y2 = n
+(where the unknowns are x and y, and n is a given positive number) in decreasing order of the positive xi.
+
+If there is no solution return [] or "[]" or "". (See "RUN SAMPLE TESTS" for examples of returns).
+
+Examples:
+solEquaStr(90005) --> "[[45003, 22501], [9003, 4499], [981, 467], [309, 37]]"
+solEquaStr(90002) --> "[]"
+Hint:
+x2 - 4 * y2 = (x - 2*y) * (x + 2*y)
+"""
+
+import math
+
+
+def sol_equa(n):
+    res = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            j = n // i
+            if (i + j) % 2 == 0 and (j - i) % 4 == 0:
+                x = (i + j) // 2
+                y = (j - i) // 4
+                res.append([x, y])
+
+    return res
+
+
+if __name__ == "__main__":
+    print(sol_equa(90005))
+    print(sol_equa(90002))
