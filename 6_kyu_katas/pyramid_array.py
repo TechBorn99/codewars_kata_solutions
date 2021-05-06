23f3b642a7564eef2d0cbe22356dd0362bd72b0b
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-05-06 19:57:32 +0200

    Added "Pyramid Array" kata solution

---
+++ 6_kyu_katas/pyramid_array.py
@@ -0,0 +1,21 @@
+"""
+Kata description:
+
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+Note: the subarrays should be filled with 1s
+"""
+
+
+def pyramid(n):
+    return [[1 for j in range(i + 1)] for i in range(n)]
+
+
+print(pyramid(0))
+print(pyramid(1))
+print(pyramid(2))
+print(pyramid(3))
