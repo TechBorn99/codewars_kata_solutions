6a8ab5730497abe6e7549d473a58d811aebfcabe
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-05-12 13:49:08 +0200

    Added "Number of People in the Bus" kata solution

---
+++ 7_kyu_katas/number_of_people_in_the_bus.py
@@ -0,0 +1,35 @@
+"""
+Kata description:
+
+Number of people in the bus
+There is a bus moving in the city, and it takes and drop some people in each bus stop.
+
+You are provided with a list (or array) of integer arrays (or tuples). Each integer array has two items which represent
+number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+
+Your task is to return number of people who are still in the bus after the last bus station (after the last array).
+Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably
+sleeping there :D
+
+Take a look on the test cases.
+
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return
+integer can't be negative.
+
+The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+"""
+
+
+def number(bus_stops):
+    ins = out = 0
+    for i in bus_stops:
+        for j in range(len(i)):
+            if j == 0:
+                ins += i[j]
+            else:
+                out += i[j]
+    return ins - out
+
+
+print(number([[10, 0], [3, 5], [5, 8]]))
+print(number([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]]))
