de9821a29e297948af169d41334e75848931b7c5
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-06-27 12:30:49 +0200

    Added "Title Case" kata solution

---
+++ 6_kyu_katas/title_case.py
@@ -0,0 +1,46 @@
+"""
+Kata description:
+
+A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the
+first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless
+it is the first word, which is always capitalised.
+
+Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The
+list of minor words will be given as a string with each word separated by a space. Your function should ignore the case
+of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+###Arguments (Haskell)
+
+First argument: space-delimited list of minor words that must always be lowercase except for the first word in the
+string.
+Second argument: the original string to be converted.
+###Arguments (Other languages)
+
+First argument (required): the original string to be converted.
+Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word
+in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+###Example
+
+title_case('a clash of KINGS', 'a an the of') # should return: 'A Clash of Kings'
+title_case('THE WIND IN THE WILLOWS', 'The In') # should return: 'The Wind in the Willows'
+title_case('the quick brown fox') # should return: 'The Quick Brown Fox'
+"""
+
+
+def title_case(title, minor_words=''):
+    temp = title.split()
+    exclude = minor_words.lower().split()
+    index = 0
+    result = []
+    for i in temp:
+        if i.lower() not in exclude or index == 0:
+            result.append(i[0].upper() + i[1:].lower())
+        else:
+            result.append(i.lower())
+        index += 1
+    return ' '.join(result)
+
+
+print(title_case('a clash of KINGS', 'a an the of'))
+print(title_case('THE WIND IN THE WILLOWS', 'The In'))
+print(title_case('the quick brown fox'))
