99a6626aacc1a0e7d20f87250b41bba8bc78abc9
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-07-29 22:27:34 +0200

    Added "Sum of all the multiples of 3 or 5" kata solution

---
+++ 7_kyu_katas/sum_of_all_the_multiples_of_3_or_5.py
@@ -0,0 +1,22 @@
+"""
+Kata description:
+
+Your task is to write function findSum.
+
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+For example:
+
+findSum(5) should return 8 (3 + 5)
+
+findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+"""
+
+
+def find(n):
+    return sum([x for x in range(n + 1) if x % 3 == 0 or x % 5 == 0])
+
+
+print(find(5))
+print(find(10))
+
