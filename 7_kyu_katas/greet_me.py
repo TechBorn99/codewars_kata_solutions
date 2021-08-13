2d56d49896296a4625d2b3f52c8c58fad15d9a25
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-08-13 23:11:08 +0200

    Added "Greet Me" kata solution

---
+++ 7_kyu_katas/greet_me.py
@@ -0,0 +1,18 @@
+"""
+Kata description:
+
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+"""
+
+
+def greet(name):
+    return 'Hello {}!'.format(name.capitalize())
+
+
+print(greet('riley'))
+print(greet('JACK'))
